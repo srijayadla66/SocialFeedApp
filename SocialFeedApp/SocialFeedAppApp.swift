db987b71019cae7e8466a231c47326b167976e97
rammohanraonamburi <namburirammohanrao2005@gmail.com>
2025-08-06 12:03:34 +0530

    Initial Commit

---
+++ SocialFeedApp/SocialFeedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialFeedAppApp.swift
+//  SocialFeedApp
+//
+//  Created by Namburi Rammohanrao on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct SocialFeedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SocialFeedAppTests/SocialFeedAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SocialFeedAppTests.swift
+//  SocialFeedAppTests
+//
+//  Created by Namburi Rammohanrao on 8/6/25.
+//
+
+import Testing
+@testable import SocialFeedApp
+
+struct SocialFeedAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
